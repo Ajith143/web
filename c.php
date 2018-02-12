c33a72bdd00eca762f41853ab4ecd4fab8fece9d
Ajith143 <36333794+Ajith143@users.noreply.github.com>
2018-02-12 19:52:31 +0530

    Create c.php

---
+++ c.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="en">
+
+<!-- Mirrored from themelooks.us/demo/socifly/html/cart.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 01 Dec 2017 13:47:38 GMT -->
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- ==== Document Title ==== -->
+    <title>SocialEngineering - Multipurpose Social Network HTML5 Template</title>
+    
+    <!-- ==== Document Meta ==== -->
+    <meta name="author" content="ThemeLooks">
+    <meta name="description" content="Multipurpose Social Network HTML5 Template">
+    <meta name="keywords" content="social media, social network, forum, shop, bootstrap, html5, css3, template, responsive, retina ready">
+
+    <!-- ==== Favicon ==== -->
+    <link rel="icon" href="favicon.png" type="image/png">
+
+    <!-- ==== Google Font ==== -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway:400,500,600,700%7CRoboto:300,400,400i,500,700">
+
+    <!-- ==== Plugins Bundle ==== -->
+    <link rel="stylesheet" href="css/plugins.min.css">
+    
+    <!-- ==== Main Stylesheet ==== -->
+    <link rel="stylesheet" href="style.css">
+    
+    <!-- ==== Responsive Stylesheet ==== -->
+    <link rel="stylesheet" href="css/responsive-style.css">
+    
+    <!-- ==== Color Scheme Stylesheet ==== -->
+    <link rel="stylesheet" href="css/colors/color-1.css" id="changeColorScheme">
+    
+    <!-- ==== Custom Stylesheet ==== -->
+    <link rel="stylesheet" href="css/custom.css">
+
+    <!-- ==== HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries ==== -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+
+    <!-- Preloader Start -->
+    <div id="preloader">
+        <div class="preloader--inner"></div>
+    </div>
+    <!-- Preloader End -->
+
+    <!-- Wrapper Start -->
+    <div class="wrapper">
+        <!-- Header Section Start -->
+        <?php  include("header.php"); ?>
+        <!-- Header Section End -->
+
+        <!-- Page Header Start -->
+        <div class="page--header pt--60 pb--60 text-center" data-bg-img="img/page-header-img/bg.jpg" data-overlay="0.85">
+            <div class="container">
+                <div class="title">
+                    <h2 class="h1 text-white">Domain</h2>
+                </div>
+
+                <ul class="breadcrumb text-gray ff--primary">
+                    <li><a href="home-1.php" class="btn-link">Home</a></li>
+                    <li class="active"><span class="text-primary">Domain</span></li>
+                </ul>
+            </div>
+        </div>
+        <!-- Page Header End -->
+		<div class="row">
+		<div class="col-md-1">
+		</div>
+		<div class="col-md-6">
+		<h3>Author Name:</h3><h6><p>Ulla Kirch-Prinz
+Peter Prinz</p></h6>
+		 </div>
+		 </div>
+		 <div class="row">
+		 <div class="col-md-1">
+		 </div>
+		 <div class="col-md-6">
+		<h3>Publisher Name:</h3><h6><p>Jones and Bartlett Publishers,</p><p> Canada. </p></h6>
+		 </div>
+		 </div>
+		 <div class="row">
+		 <div class="col-md-1">
+		 </div>
+		 <div class="col-md-10">
+		<h3>Introduction:</h3>
+		
+		<h6><div class="text" ><p align="justify">The C++ language definition is based on the American National Standards Institute ANSI Standard X3J16. This standard also complies with ISO norm 14882, which was ratified by the International Standardization Organization in 1998. The C++ programming language is thus platform-independent in the main with a majority of C++ compilers providing ANSI support. New elements of the C++ language, such as exception handling and templates, are supported by most of the major compilers. Visit the Jones and Bartlett web site at www.jbpub.com for a listing of compilers available for this text. The C++ language definition is based on the American National Standards Institute ANSI Standard X3J16. This standard also complies with ISO norm 14882, which was ratified by the International Standardization Organization in 1998. The C++ programming language is thus platform-independent in the main with a majority of C++ compilers providing ANSI support. New elements of the C++ language, such as exception handling and templates, are supported by most of the major compilers. Visit the Jones and Bartlett web site at www.jbpub.com for a listing of compilers available for this text.</div></p></h6>
+		 </div>
+		  </div>
+		
+       
+
+        <!-- Footer Section Start -->
+        <?php  include("footer.php"); ?>
+        <!-- Footer Section End -->
+    </div>
+    <!-- Wrapper End -->
+
+    <!-- Back To Top Button Start -->
+    <div id="backToTop">
+        <a href="#" class="btn"><i class="fa fa-caret-up"></i></a>
+    </div>
+    <!-- Back To Top Button End -->
+
+    <!-- ==== Plugins Bundle ==== -->
+    <script src="js/plugins.min.js"></script>
+
+    <!-- ==== Color Switcher Plugin ==== -->
+   
+
+    <!-- ==== Main Script ==== -->
+    <script src="js/main.js"></script>
+
+</body>
+
+<!-- Mirrored from themelooks.us/demo/socifly/html/cart.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 01 Dec 2017 13:47:40 GMT -->
+</html>
