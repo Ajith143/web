4ae0cb76ea361cc1261a677a9821451fdc4b8a20
Ajith143 <36333794+Ajith143@users.noreply.github.com>
2018-02-12 20:05:22 +0530

    Create post.php

---
+++ post.php
@@ -0,0 +1,191 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="en">
+
+<!-- Mirrored from themelooks.us/demo/socifly/html/member-activity-personal.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 01 Dec 2017 13:48:41 GMT -->
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- ==== Document Title ==== -->
+    <title>SocialEngineering - Multipurpose Social Network HTML5 Template</title>
+    
+    <!-- ==== Document Meta ==== -->
+    <meta name="author" content="ThemeLooks">
+    <meta name="description" content="Multipurpose Social Network HTML5 Template">
+    <meta name="keywords" content="social media, social network, forum, shop, bootstrap, html5, css3, template, responsive, retina ready">
+
+    <!-- ==== Favicon ==== -->
+    <link rel="icon" href="favicon.png" type="image/png">
+
+    <!-- ==== Google Font ==== -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway:400,500,600,700%7CRoboto:300,400,400i,500,700">
+
+    <!-- ==== Plugins Bundle ==== -->
+    <link rel="stylesheet" href="css/plugins.min.css">
+    
+    <!-- ==== Main Stylesheet ==== -->
+    <link rel="stylesheet" href="style.css">
+    
+    <!-- ==== Responsive Stylesheet ==== -->
+    <link rel="stylesheet" href="css/responsive-style.css">
+    
+    <!-- ==== Color Scheme Stylesheet ==== -->
+    <link rel="stylesheet" href="css/colors/color-1.css" id="changeColorScheme">
+    
+    <!-- ==== Custom Stylesheet ==== -->
+    <link rel="stylesheet" href="css/custom.css">
+
+    <!-- ==== HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries ==== -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+
+    <!-- Preloader Start -->
+    <div id="preloader">
+        <div class="preloader--inner"></div>
+    </div>
+    <!-- Preloader End -->
+
+    <!-- Wrapper Start -->
+    <div class="wrapper">
+        <!-- Header Section Start -->
+        <?php  include("header.php"); ?>
+        <!-- Header Section End -->
+
+        <!-- Cover Header Start -->
+        <div class="cover--header pt--80 text-center" data-bg-img="img/cover-header-img/bg-01.jpg" data-overlay="0.6" data-overlay-color="white">
+            <div class="container">
+                <div class="cover--avatar online" data-overlay="0.3" data-overlay-color="primary">
+                    <img src="img/cover-header-img/avatar-01.jpg" alt="">
+                </div>
+
+                <div class="cover--user-name">
+                    <h2 class="h3 fw--600">Eileen K. Ruiz</h2>
+					<p>Full Stack Web Developer of 6 years, Programmer of 8 years</p>
+                </div>
+                  <div class="main--content-inner drop--shadow">
+                            <!-- Content Nav Start -->
+                            <div class="content--nav pb--30">
+                                <ul class="nav ff--primary fs--14 fw--500 bg-lighter">
+                                    <li><a href="member-activity-personal.php">Profile</a></li>
+									 <li><a href="domain_achievers.php">Domain</a></li>
+									 <li><a href="post.php">Posts</a></li>
+                                </ul>
+                            </div>
+                <div class="cover--user-desc fw--400 fs--18 fstyle--i text-darkest">
+				<div class="post--title mt--10">
+				  <div class="post--meta">
+				  <ul class="nav">
+                                                    <li>
+                                                        <a href="#">
+                                                            <img src="img/activity-img/gallery-embed-01.jpg" alt="">
+                                                            <span>Eileen K. Ruiz</span>
+                                                        </a>
+                                                    </li>
+				  <div class="post--img">
+                                           <img src="img/posts-img/post-item-01.jpg" alt="">
+                                        </div>
+                                                
+                                            
+<ul class="nav">
+                                                   
+                                                    <li>
+                                                        <a href="#">
+                                                            <i class=" fa fa-eye"></i>
+                                                            <span><h6>Viewership</h6></span>
+                                                        </a>
+                                                    </li>
+                                                    <li>
+													 <li>
+                                                        <a href="comment1.php">
+                                                            <i class="mr--8 fa fa-comments-o"></i>
+                                                            <span><h6> Comments</h6></span>
+                                                        </a>
+                                                    </li>
+                                                     </li>
+                                                </ul>
+												</div>
+												<div class="post--title mt--10">
+                                                <h3 class="h6">
+                                                    <a href="blog-details.html" class="btn-link">This Is the Place I Was Telling You About</a>
+                                                </h3>
+                                            </div>
+											  <div class="post--content text-darker mt--10">
+                                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley.</p>
+                                            </div>
+										<div class="post--meta">
+				  <ul class="nav">
+                                                    <li>
+                                                        <a href="#">
+                                                            <img src="img/activity-img/gallery-embed-01.jpg" alt="">
+                                                            <span>Eileen K. Ruiz</span>
+                                                        </a>
+                                                    </li>
+				  <div class="post--img">
+                                           <img src="img/activity-img/gallery-embed-02.jpg" alt="">
+                                        </div>
+                                                
+                                            
+<ul class="nav">
+                                                   
+                                                    <li>
+                                                        <a href="#">
+                                                            <i class=" fa fa-eye"></i>
+                                                            <span><h6>Viewership</h6></span>
+                                                        </a>
+                                                    </li>
+                                                    <li>
+                                                        <a href="comment2.php">
+                                                            <i class="mr--8 fa fa-comments-o"></i>
+                                                            <span><h6> Comment</h6></span>
+                                                        </a>
+                                                    </li>
+                                                </ul>
+												</div>		
+												<div class="post--title mt--10">
+                                                <h3 class="h6">
+                                                    <a href="blog-details.html" class="btn-link">This Is the Place I Was Telling You About</a>
+                                                </h3>
+                                            </div>
+											  <div class="post--content text-darker mt--10">
+                                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley.</p>
+                                            </div>	
+													
+                </div>
+            </div>
+        </div>
+        <!-- Cover Header End -->
+
+        <!-- Page Wrapper Start -->
+        
+        <!-- Page Wrapper End -->
+
+        <!-- Footer Section Start -->
+       <?php  include("footer.php"); ?>
+        <!-- Footer Section End -->
+    </div>
+    <!-- Wrapper End -->
+
+    <!-- Back To Top Button Start -->
+    <div id="backToTop">
+        <a href="#" class="btn"><i class="fa fa-caret-up"></i></a>
+    </div>
+    <!-- Back To Top Button End -->
+
+    <!-- ==== Plugins Bundle ==== -->
+    <script src="js/plugins.min.js"></script>
+
+    <!-- ==== Color Switcher Plugin ==== -->
+   
+
+    <!-- ==== Main Script ==== -->
+    <script src="js/main.js"></script>
+
+</body>
+
+<!-- Mirrored from themelooks.us/demo/socifly/html/member-activity-personal.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 01 Dec 2017 13:48:44 GMT -->
+</html>
