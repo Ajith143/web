2bce5c4a4dcf6f2d8a074a520c62fd7a5f931f6e
Ajith143 <36333794+Ajith143@users.noreply.github.com>
2018-02-12 19:59:11 +0530

    Rename forum.php to forum1.php

---
+++ forum1.php
@@ -0,0 +1,258 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="en">
+
+<!-- Mirrored from themelooks.us/demo/socifly/html/activity.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 01 Dec 2017 13:48:09 GMT -->
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- ==== Document Title ==== -->
+    <title>SocialEngineering- Multipurpose Social Network HTML5 Template</title>
+    
+    <!-- ==== Document Meta ==== -->
+    <meta name="author" content="ThemeLooks">
+    <meta name="description" content="Multipurpose Social Network HTML5 Template">
+    <meta name="keywords" content="social media, social network, forum, shop, bootstrap, html5, css3, template, responsive, retina ready">
+
+    <!-- ==== Favicon ==== -->
+    <link rel="icon" href="favicon.png" type="image/png">
+
+    <!-- ==== Google Font ==== -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway:400,500,600,700%7CRoboto:300,400,400i,500,700">
+
+    <!-- ==== Plugins Bundle ==== -->
+    <link rel="stylesheet" href="css/plugins.min.css">
+    
+    <!-- ==== Main Stylesheet ==== -->
+    <link rel="stylesheet" href="style.css">
+    
+    <!-- ==== Responsive Stylesheet ==== -->
+    <link rel="stylesheet" href="css/responsive-style.css">
+    
+    <!-- ==== Color Scheme Stylesheet ==== -->
+    <link rel="stylesheet" href="css/colors/color-1.css" id="changeColorScheme">
+    
+    <!-- ==== Custom Stylesheet ==== -->
+    <link rel="stylesheet" href="css/custom.css">
+
+    <!-- ==== HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries ==== -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+
+    <!-- Preloader Start -->
+    <div id="preloader">
+        <div class="preloader--inner"></div>
+    </div>
+    <!-- Preloader End -->
+
+    <!-- Wrapper Start -->
+    <div class="wrapper">
+        <!-- Header Section Start -->
+        <?php  include("header.php"); ?>
+        <!-- Header Section End -->
+<div class="cover--header pt--80 text-center" data-bg-img="img/cover-header-img/bg-01.jpg" data-overlay="0.6" data-overlay-color="white">
+            <div class="container">
+                <div class="cover--avatar online" data-overlay="0.3" data-overlay-color="primary">
+                    <img src="img/cover-header-img/avatar-01.jpg" alt="">
+                </div>
+
+                <div class="cover--user-name">
+                    <h2 class="h3 fw--600">Samuel C. Azevedo</h2>
+                </div>
+
+                <div class="cover--user-activity">
+                    <p><i class="fa mr--8 fa-clock-o"></i>Active 1 year 9 monts ago</p>
+                </div>
+            </div>
+        </div>
+		
+		
+		<div class="container">
+                <div class="row">
+                    <!-- Main Content Start -->
+                    <div class="main--content col-md-12 pb--60" data-trigger="stickyScroll">
+                        <div class="main--content-inner drop--shadow">
+                            <!-- Content Nav Start -->
+                            <div class="content--nav pb--30">
+                                <ul class="nav ff--primary fs--14 fw--500 bg-lighter">
+                                    <li class="active"><a href="activity1.php">Recent posts</a></li>
+                                    <li><a href="member-groups.php">Groups</a></li>
+                                    <li><a href="member-forum-topics.php">Forum</a></li>
+                                    <li><a href="member-media-all.php">Media</a></li>
+                                </ul>
+                            </div>
+                       
+					    <section class="page--wrapper pt--80 pb--20">
+            
+					   <div class="container">
+                <div class="row">
+                    <!-- Main Content Start -->
+                    <div class="main--content col-md-8 pb--60" data-trigger="stickyScroll">
+                        <div class="main--content-inner drop--shadow">
+                            <!-- Filter Nav Start -->
+                            
+                            <!-- Filter Nav End -->
+
+                            <!-- Activity List Start -->
+                            <div class="activity--list">
+                                <!-- Activity Items Start -->
+                                <ul class="activity--items nav">
+                                    <li>
+                                        <!-- Activity Item Start -->
+                                        <div class="activity--item">
+                                            <div class="activity--avatar">
+                                                <a href="#">
+                                                    <img src="img/cart-img/c++pic.png" alt=""> </a>                                               </a>                                            </div>
+
+                                            <div class="activity--info fs--14">
+                                                <div class="activity--header">
+                                                    <p><a href="#">Programming In C&C++</a></p>
+                                                </div>
+                                                <div class="activity--content">
+                                                    <div class="img--embed">
+													<img src="img/cart-img/c++pic.png" alt="">
+														</a>
+														</div>
+														 <div class="activity--content">
+                                                    <p>C++ is a general-purpose object-oriented programming (OOP) language, developed by Bjarne Stroustrup, and is an extension of the C language. It is therefore possible to code C++ in a "C style" or "object-oriented style." ... However, it was renamed C++ in 1983.</p>
+                                                </div>
+												<div class="activity--header">
+                                                    <p><a href="forum_discussion.php">Any Suggestions</a></p>
+                                                </div>                                                    
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <!-- Activity Item End -->
+                                    </li>
+                                    <li>
+                                        <!-- Activity Item Start -->
+                                        <div class="activity--item">
+                                            <div class="activity--avatar">
+                                                <a href="#">
+                                                    <img src="img/cart-img/java.jpg" alt="">
+											 </a>
+												</div>
+
+                                            <div class="activity--info fs--14">
+                                                <div class="activity--header">
+                                                    <p><a href="#">The Complete Reference In Java</a></p>
+                                                </div>
+                                                <div class="activity--content">
+                                                    <p>Java is a general-purpose computer programming language that is concurrent, class-based, object-oriented, and specifically designed to have as few implementation dependencies as possible.</p>
+                                                </div>
+
+                                               
+                                            </div>
+                                        </div>
+                                        <!-- Activity Item End -->
+                                    </li>
+                                    <li>
+                                        <!-- Activity Item Start -->
+                                        <div class="activity--item">
+                                            <div class="activity--avatar">
+                                                <a href="member-activity-personal.html">
+                                                     <img src="img/cart-img/php& mysql.jpg" alt="">                                               </a>                                            </div>
+
+                                            <div class="activity--info fs--14">
+                                                <div class="activity--header">
+                                                    <p><a href="#">Beginning PHP and MYSQL</a></p>
+                                                </div>
+
+                                                <div class="activity--content">
+                                                    <p>PHP and MySQL Development. PHP is a fast and feature-rich open source scripting language used to develop Web Applications or Internet / Intranet Applications. ... When developed correctly, applications can be built with clean and simple usability, complex functionality, speed, power and scalability.</p>
+                                                </div>
+
+                                                <div class="activity--comments fs--12">
+                                                    <ul class="acomment--items nav">
+                                                        <li>
+                                                            <div class="acomment--item clearfix">
+                                                                <div class="acomment--avatar">
+                                                                    <a href="member-activity-personal.html">
+                                                                        <img src="img/cart-img/phython.jpeg" alt="">                                                                    </a>                                                               
+																    </div>
+                                                                <div class="acomment--info">
+                                                                    <div class="acomment--header">
+                                                                        <p><a href="#">Python</a></p>
+                                                                    </div>
+																	   <div class="activity--content">
+                                                                        <p>The DML vocabulary is used to retrieve and manipulate data, while DDL statements are for defining and modifying database structures. ... The security statements are used to control database access as well as to create user roles and permissions. SQL syntax is the coding format used in writing statements.</p>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                        </li>
+                                                    </ul>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <!-- Activity Item End -->
+                                    </li>
+                                    
+                                    
+                                </ul>
+                                <!-- Activity Items End -->
+                            </div>
+                            <!-- Activity List End -->
+                        </div>
+
+                        <!-- Load More Button Start -->
+                        <div class="load-more--btn pt--30 text-center">
+                            <a href="#" class="btn btn-animate">
+                                <span>See More Activities<i class="fa ml--10 fa-caret-right"></i></span>                            </a>                        </div>
+                        <!-- Load More Button End -->
+                    </div>
+                    <!-- Main Content End -->
+
+                    <!-- Main Sidebar Start -->
+                    
+                    <!-- Main Sidebar End -->
+                </div>
+            </div>
+					   
+					       </section>
+					   
+					   
+					   
+                        </div>
+                    </div>
+                    <!-- Main Content End -->
+
+                    <!-- Main Sidebar Start -->
+                    
+                    <!-- Main Sidebar End -->
+                </div>
+            </div>
+        <!-- Page Header Start -->
+        
+        <!-- Page Header End -->
+
+        <!-- Page Wrapper Start -->
+       
+    
+        <!-- Page Wrapper End -->
+
+    </div>
+    <!-- Wrapper End -->
+
+    <!-- Back To Top Button Start -->
+    <div id="backToTop">
+        <a href="#" class="btn"><i class="fa fa-caret-up"></i></a>    </div>
+    <!-- Back To Top Button End -->
+
+    <!-- ==== Plugins Bundle ==== -->
+    <script src="js/plugins.min.js"></script>
+
+    <!-- ==== Color Switcher Plugin ==== -->
+    
+
+    <!-- ==== Main Script ==== -->
+    <script src="js/main.js"></script>
+
+</body>
+
+<!-- Mirrored from themelooks.us/demo/socifly/html/activity.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 01 Dec 2017 13:48:22 GMT -->
+</html>
