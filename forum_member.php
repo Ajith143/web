621ff2e3c7bb769450bf535e6de18a0fdb5c4de5
Ajith143 <36333794+Ajith143@users.noreply.github.com>
2018-02-12 19:59:38 +0530

    Create forum_member.php

---
+++ forum_member.php
@@ -0,0 +1,248 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="en">
+
+<!-- Mirrored from themelooks.us/demo/socifly/html/activity.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 01 Dec 2017 13:48:09 GMT -->
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- ==== Document Title ==== -->
+    <title>SocialEngineering- Multipurpose Social Network HTML5 Template</title>
+    
+    <!-- ==== Document Meta ==== -->
+    <meta name="author" content="ThemeLooks">
+    <meta name="description" content="Multipurpose Social Network HTML5 Template">
+    <meta name="keywords" content="social media, social network, forum, shop, bootstrap, html5, css3, template, responsive, retina ready">
+
+    <!-- ==== Favicon ==== -->
+    <link rel="icon" href="favicon.png" type="image/png">
+
+    <!-- ==== Google Font ==== -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway:400,500,600,700%7CRoboto:300,400,400i,500,700">
+
+    <!-- ==== Plugins Bundle ==== -->
+    <link rel="stylesheet" href="css/plugins.min.css">
+    
+    <!-- ==== Main Stylesheet ==== -->
+    <link rel="stylesheet" href="style.css">
+    
+    <!-- ==== Responsive Stylesheet ==== -->
+    <link rel="stylesheet" href="css/responsive-style.css">
+    
+    <!-- ==== Color Scheme Stylesheet ==== -->
+    <link rel="stylesheet" href="css/colors/color-1.css" id="changeColorScheme">
+    
+    <!-- ==== Custom Stylesheet ==== -->
+    <link rel="stylesheet" href="css/custom.css">
+
+    <!-- ==== HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries ==== -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+
+    <!-- Preloader Start -->
+    <div id="preloader">
+        <div class="preloader--inner"></div>
+    </div>
+    <!-- Preloader End -->
+
+    <!-- Wrapper Start -->
+    <div class="wrapper">
+        <!-- Header Section Start -->
+        <?php  include("header.php"); ?>
+        <!-- Header Section End -->
+
+        <!-- Page Header Start -->
+        <div class="page--header pt--60 pb--60 text-center" data-bg-img="img/page-header-img/bg.jpg" data-overlay="0.85">
+            <div class="container">
+                <div class="title">
+                    <h2 class="h1 text-white">Forum</h2>
+                </div>
+
+                <ul class="breadcrumb text-gray ff--primary">
+                    <li><a href="home-1.php" class="btn-link">Home</a></li>
+                    <li class="active"><span class="text-primary">Forum</span></li>
+                </ul>
+            </div>
+        </div>
+        <!-- Page Header End -->
+
+        <!-- Page Wrapper Start -->
+        <section class="page--wrapper pt--80 pb--20">
+            <div class="container">
+                <div class="row">
+                    <!-- Main Content Start -->
+                    <div class="main--content col-md-12 pb--60" data-trigger="stickyScroll">
+                        <div class="main--content-inner drop--shadow">
+                            
+                            <!-- Activity List Start -->
+                            <div class="activity--list">
+                                <!-- Activity Items Start -->
+                                <ul class="activity--items nav">
+                                    
+                                    <li>
+                                        <!-- Activity Item Start -->
+                                        <div class="activity--item">
+                                            <div class="activity--avatar">
+                                                <a href="member-activity-personal.php">
+                                                    <img src="img/activity-img/avatar-02.jpg" alt="">                                                </a>                                            </div>
+
+                                            <div class="activity--info fs--14">
+                                                <div class="activity--header">
+                                                    <p><a href="fourm.php"><h4>Samuel C. Azevedo</h4></a></p>
+													<p> <a href="fourm.php">Recent Posts</a></p>
+                                                </div>
+                                                <div class="activity--content">
+                                                    <p>I'm a full-stack web-developer since 1995. My main focus is on PHP but I know JavaScript, Node.js, HTML, DOM, Frameworks, Composer, Hosting, ....</p>
+                                                </div>
+                                        </div>
+                                        <!-- Activity Item End -->
+                                    </li>
+                                    <li>
+                                        <!-- Activity Item Start -->
+                                        <div class="activity--item">
+                                            <div class="activity--avatar">
+                                                <a href="member-activity-personal.php">
+                                                    <img src="img/activity-img/avatar-03.jpg" alt="">                                                </a>                                            </div>
+
+                                            <div class="activity--info fs--14">
+                                                <div class="activity--header">
+                                                    <p><a href="fourm.php"><h4>Denise R. Sherman</h4></a></p>
+													<p><a href="fourm.php">Recent Posts</a></p>
+                                                </div>
+
+                                                <div class="activity--content">
+                                                    <p>Done a heck of a lot of programming and software design since I wrote my first on a Sinclair ZX spectrum in 1987. Not enough of it has been made open source...</p>
+                                                </div>
+                                        </div>
+                                        <!-- Activity Item End -->
+                                    </li>
+									<li>
+                                        <!-- Activity Item Start -->
+                                        <div class="activity--item">
+                                            <div class="activity--avatar">
+                                                <a href="member-activity-personal.php">
+                                                    <img src="img/activity-img/avatar-02.jpg" alt="">                                                
+													</a>                                            </div>
+
+                                            <div class="activity--info fs--14">
+                                                <div class="activity--header">
+                                                    <p><a href="fourm.php"><h4>Samuel C. Azevedo</h4></a></p>
+													<p> <a href="fourm.php">Recent Posts</a></p>
+                                                </div>
+                                                <div class="activity--content">
+                                                    <p>I'm a full-stack web-developer since 1995. My main focus is on PHP but I know JavaScript, Node.js, HTML, DOM, Frameworks, Composer, Hosting, ....</p>
+                                                </div>
+                                        </div>
+                                        <!-- Activity Item End -->
+                                    </li>
+                                    <li>
+                                        <!-- Activity Item Start -->
+                                        <div class="activity--item">
+                                            <div class="activity--avatar">
+                                                <a href="member-activity-personal.php">
+                                                    <img src="img/activity-img/avatar-03.jpg" alt="">                                                
+													</a>                                            </div>
+
+                                            <div class="activity--info fs--14">
+                                                <div class="activity--header">
+                                                    <p><a href="fourm.php"><h4>Denise R. Sherman</h4></a></p>
+													<p><a href="fourm.php">Recent Posts</a></p>
+                                                </div>
+
+                                                <div class="activity--content">
+                                                    <p>Done a heck of a lot of programming and software design since I wrote my first on a Sinclair ZX spectrum in 1987. Not enough of it has been made open source...</p>
+                                                </div>
+                                        </div>
+                                        <!-- Activity Item End -->
+                                    </li>
+                                   <li>
+                                        <!-- Activity Item Start -->
+                                        <div class="activity--item">
+                                            <div class="activity--avatar">
+                                                <a href="member-activity-personal.php">
+                                                    <img src="img/activity-img/avatar-02.jpg" alt="">                                               
+													 </a>                                            </div>
+
+                                            <div class="activity--info fs--14">
+                                                <div class="activity--header">
+                                                    <p><a href="fourm.php"><h4>Samuel C. Azevedo</h4></a></p>
+													<p> <a href="fourm.php">Recent Posts</a></p>
+                                                </div>
+                                                <div class="activity--content">
+                                                    <p>I'm a full-stack web-developer since 1995. My main focus is on PHP but I know JavaScript, Node.js, HTML, DOM, Frameworks, Composer, Hosting, ....</p>
+                                                </div>
+                                        </div>
+                                        <!-- Activity Item End -->
+                                    </li>
+                                    <li>
+                                        <!-- Activity Item Start -->
+                                        <div class="activity--item">
+                                            <div class="activity--avatar">
+                                                <a href="member-activity-personal.php">
+                                                    <img src="img/activity-img/avatar-03.jpg" alt="">                                                </a>                                            </div>
+
+                                            <div class="activity--info fs--14">
+                                                <div class="activity--header">
+                                                    <p><a href="fourm.php"><h4>Denise R. Sherman</h4></a></p>
+													<p><a href="fourm.php">Recent Posts</a></p>
+                                                </div>
+
+                                                <div class="activity--content">
+                                                    <p>Done a heck of a lot of programming and software design since I wrote my first on a Sinclair ZX spectrum in 1987. Not enough of it has been made open source...</p>
+                                                </div>
+                                        </div>
+                                        <!-- Activity Item End -->
+                                    </li>
+                                   
+                                   
+                                </ul>
+                                <!-- Activity Items End -->
+                            </div>
+                            <!-- Activity List End -->
+                        </div>
+
+                        <!-- Load More Button Start -->
+                        <div class="load-more--btn pt--30 text-center">
+                            <a href="#" class="btn btn-animate">
+                                <span>See More Activities<i class="fa ml--10 fa-caret-right"></i></span>                            </a>                        </div>
+                        <!-- Load More Button End -->
+                    </div>
+                    <!-- Main Content End -->
+
+                    <!-- Main Sidebar Start -->
+                    
+                    <!-- Main Sidebar End -->
+                </div>
+            </div>
+        </section>
+        <!-- Page Wrapper End -->
+
+        <!-- Footer Section Start -->
+        <?php  include("footer.php"); ?>
+        <!-- Footer Section End -->
+    </div>
+    <!-- Wrapper End -->
+
+    <!-- Back To Top Button Start -->
+    <div id="backToTop">
+        <a href="#" class="btn"><i class="fa fa-caret-up"></i></a>    </div>
+    <!-- Back To Top Button End -->
+
+    <!-- ==== Plugins Bundle ==== -->
+    <script src="js/plugins.min.js"></script>
+
+    <!-- ==== Color Switcher Plugin ==== -->
+    
+
+    <!-- ==== Main Script ==== -->
+    <script src="js/main.js"></script>
+
+</body>
+
+<!-- Mirrored from themelooks.us/demo/socifly/html/activity.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 01 Dec 2017 13:48:22 GMT -->
+</html>
