e79a45d6ca27d98a62e71766038485c7cbf21a31
Ajith143 <36333794+Ajith143@users.noreply.github.com>
2018-02-12 20:01:32 +0530

    Create header.php

---
+++ header.php
@@ -0,0 +1,105 @@
+     <header class="header--section style--1">
+            <!-- Header Topbar Start -->
+            <div class="header--topbar bg-black">
+                <div class="container">
+                    <!-- Header Topbar Links Start -->
+                    
+                    <!-- Header Topbar Links End -->
+
+                    <!-- Header Topbar Social Start -->
+                    <ul class="header--topbar-social nav float--left hidden-xs">
+                        <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                        <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                        <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+                        <li><a href="#"><i class="fa fa-rss"></i></a></li>
+                        <li><a href="#"><i class="fa fa-youtube"></i></a></li>
+                    </ul>
+                    <!-- Header Topbar Social End -->
+
+                    <!-- Header Topbar Links Start -->
+                    <ul class="header--topbar-links nav ff--primary float--right">
+                        
+                        <li>
+                            <a href="myaccount1.php" class="btn-link">
+                                <i class="fa mr--8 fa-user-o"></i>
+                                <span>My Account</span>
+                            </a>
+                        </li>
+                    </ul>
+                    <!-- Header Topbar Links End -->
+                </div>
+            </div>
+            <!-- Header Topbar End -->
+
+            <!-- Header Navbar Start -->
+			
+            <div class="header--navbar navbar bg-black" data-trigger="sticky">
+                <div class="container">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle style--1 collapsed" data-toggle="collapse" data-target="#headerNav">
+                            <span class="sr-only">Toggle Navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+
+                        <!-- Header Navbar Logo Start -->
+                        <div class="header--navbar-logo navbar-brand">
+                            <a href="index.html">
+                                <img src="img/social white1.png" class="normal" alt="">
+                                <img src="img/social3 black1.png" class="sticky" alt="">
+                            </a>
+                        </div>
+                        <!-- Header Navbar Logo End -->
+                    </div>
+
+                    <div id="headerNav" class="navbar-collapse collapse float--right">
+                        <!-- Header Nav Links Start -->
+                        <ul class="header--nav-links style--1 nav ff--primary">
+                            <li class="dropdown active">
+                                <a href="home-1.php">
+                                    <span> Home</span>
+                                    
+                                </a>
+
+                                
+                            </li>
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                                    <span>Community</span>
+                                    <i class="fa fa-caret-down"></i>
+                                </a>
+
+                                <ul class="dropdown-menu">
+                                    <li><a href="activity1.php"><span>Activity</span></a></li>
+                                    <li><a href="members.php"><span>Members</span></a></li>
+                                    <li><a href="groups.php"><span>Groups</span></a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="fourm_members.php">Forums</a>
+                                </a>
+                            </li>
+                            <li class="dropdown ">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                                    <span>Knowledge</span>
+                                    <i class="fa fa-caret-down"></i>
+                                </a>
+
+                                <ul class="dropdown-menu">
+                                            <li><a href="blog-sidebar-left.php">Domain</a></li>
+				                </ul>
+                            </li>
+                            <li class="dropdown">
+                                <a href="achiever2.php">
+                                     Achiever </a>
+
+                               
+                            </li>
+                            
+                        <!-- Header Nav Links End -->
+                    </div>
+                </div>
+            </div>
+            <!-- Header Navbar End -->
+        </header>
