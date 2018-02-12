4a9b0b9b53068d339da765ed93394d5fa249aaa3
Ajith143 <36333794+Ajith143@users.noreply.github.com>
2018-02-12 20:06:39 +0530

    Create unemployee.php

---
+++ unemployee.php
@@ -0,0 +1,284 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="en">
+
+<!-- Mirrored from themelooks.us/demo/socifly/html/contact.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 01 Dec 2017 13:50:17 GMT -->
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- ==== Document Title ==== -->
+    <title>SocialEngineering - Multipurpose Social Network HTML5 Template</title>
+    
+    <!-- ==== Document Meta ==== -->
+    <meta name="author" content="ThemeLooks">
+    <meta name="description" content="Multipurpose Social Network HTML5 Template">
+    <meta name="keywords" content="social media, social network, forum, shop, bootstrap, html5, css3, template, responsive, retina ready">
+
+    <!-- ==== Favicon ==== -->
+    <link rel="icon" href="favicon.png" type="image/png">
+
+    <!-- ==== Google Font ==== -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway:400,500,600,700%7CRoboto:300,400,400i,500,700">
+
+    <!-- ==== Plugins Bundle ==== -->
+    <link rel="stylesheet" href="css/plugins.min.css">
+    
+    <!-- ==== Main Stylesheet ==== -->
+    <link rel="stylesheet" href="style.css">
+    
+    <!-- ==== Responsive Stylesheet ==== -->
+    <link rel="stylesheet" href="css/responsive-style.css">
+    
+    <!-- ==== Color Scheme Stylesheet ==== -->
+    <link rel="stylesheet" href="css/colors/color-1.css" id="changeColorScheme">
+    
+    <!-- ==== Custom Stylesheet ==== -->
+    <link rel="stylesheet" href="css/custom.css">
+
+    <!-- ==== HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries ==== -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+
+    <!-- Preloader Start -->
+    <div id="preloader">
+        <div class="preloader--inner"></div>
+    </div>
+    <!-- Preloader End -->
+
+    <!-- Wrapper Start -->
+    <div class="wrapper">
+        <!-- Header Section Start -->
+        <header class="header--section style--1">
+            <!-- Header Topbar Start -->
+            <div class="header--topbar bg-black">
+                <div class="container">
+                    <!-- Header Topbar Links Start -->
+                    
+                    <!-- Header Topbar Links End -->
+
+                    <!-- Header Topbar Social Start -->
+                    <ul class="header--topbar-social nav float--left hidden-xs">
+                        <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                        <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                        <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+                        <li><a href="#"><i class="fa fa-rss"></i></a></li>
+                        <li><a href="#"><i class="fa fa-youtube"></i></a></li>
+                    </ul>
+                    <!-- Header Topbar Social End -->
+
+                    <!-- Header Topbar Links Start -->
+                    <ul class="header--topbar-links nav ff--primary float--right">
+                        
+                        <li>
+                            <a href="#" class="btn-link">
+                                <i class="fa mr--8 fa-user-o"></i>
+                                <span>My Account</span>
+                            </a>
+                        </li>
+                    </ul>
+                    <!-- Header Topbar Links End -->
+                </div>
+            </div>
+            <!-- Header Topbar End -->
+
+            <!-- Header Navbar Start -->
+            <div class="header--navbar navbar bg-black" data-trigger="sticky">
+                <div class="container">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#headerNav">
+                            <span class="sr-only">Toggle Navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+
+                        <!-- Header Navbar Logo Start -->
+                        <div class="header--navbar-logo navbar-brand">
+                            <a href="index.php">
+                                <img src="img/social white1.png" class="normal" alt="">
+                                <img src="img/social3 black1.png" class="sticky" alt="">
+                            </a>
+                        </div>
+                        <!-- Header Navbar Logo End -->
+                    </div>
+
+                    <div id="headerNav" class="navbar-collapse collapse float--right">
+                        <!-- Header Nav Links Start -->
+                        <ul class="header--nav-links style--1 nav ff--primary">
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                                    <span>Registration</span>
+                                    <i class="fa fa-caret-down"></i>
+                                </a>
+								
+                                <ul class="dropdown-menu">
+                                    <li><a href="myaccount1.php"><span>Student</span></a></li>
+                                    <li><a href="college.php"><span>College</span></a></li>
+                                    <li><a href="unemployees.php"><span>Unemployees</span></a></li>
+									<li><a href="experts.php"><span>Experts</span></a></li>
+                                </ul>
+
+                                
+                            </li>
+                            <li class="dropdown">
+                                <a href="#" >
+                                   login
+                                </a>
+
+                                
+                            </li>
+                            
+                            
+                            
+                           
+                        <!-- Header Nav Links End -->
+                    </div>
+                </div>
+            </div>
+            <!-- Header Navbar End -->
+        </header>
+        <!-- Header Section End -->
+
+        <!-- Page Header Start -->
+        
+        <!-- Page Header End -->
+
+        <!-- Contact Section Start -->
+        <div class="contact--section pt--80 pb--20">
+            <div class="container">
+                <!-- Map Start -->
+                
+                <!-- Map End -->
+
+                <div class="row">
+                    
+
+                    <div class="col-md-12 pb--60">
+                        <!-- Contact Form Start -->
+                        <div class="contact--form" data-form="ajax">
+                            <div class="contact--title">
+                                <h3 class="h4">Unemployees Registration</h3>
+                            </div>
+
+                           
+
+                            <form action="#" method="post">
+                                <div class="row gutter--20">
+                                    <div class="col-xs-6 col-xxs-12">
+                                        <div class="form-group">
+                                            <input type="text" name="name" placeholder="Name *" class="form-control" required>
+                                        </div>
+                                    </div>
+									</div>
+									<div class="row gutter--20">
+                                    <div class="col-xs-6 col-xxs-12">
+                                        <div class="form-group">
+                                            <input type="text" name="name" placeholder="College name *" class="form-control" required>
+                                        </div>
+                                    </div>
+									</div>
+									<div class="row gutter--20">
+                                    <div class="col-xs-6 col-xxs-12">
+                                        <div class="form-group">
+                                            <input type="text" name="name" placeholder="Year of Passed *" class="form-control" required>
+                                        </div>
+                                    </div>
+									</div>
+									<div class="row gutter--20">
+                                    <div class="col-xs-6 col-xxs-12">
+                                        <div class="form-group">
+                                            <input type="text" name="name" placeholder="Department *" class="form-control" required>
+                                        </div>
+                                    </div>
+									</div>
+									
+                                    <div class="row gutter--20">
+                                    <div class="col-xs-6 col-xxs-12">
+                                        <div class="form-group">
+                                            <input type="email" name="email" placeholder="E-mail *" class="form-control" required>
+                                        </div>
+                                    </div>
+									</div>
+									<div class="row gutter--20">
+                                    <div class="col-xs-6 col-xxs-12">
+                                        <div class="form-group">
+                                            <input type="password" name="name" placeholder="Password *" class="form-control" required>
+                                        </div>
+                                    </div>
+									</div>
+									<div class="row gutter--20">
+                                    <div class="col-xs-6 col-xxs-12">
+                                        <div class="form-group">
+                                            <input type="password" name="name" placeholder="Confirm password *" class="form-control" required>
+                                        </div>
+                                    </div>
+									</div>
+									
+                                   <div class="row gutter--20">
+                                   <div class="col-xs-6 col-xxs-12">
+                                        <div class="form-group">
+                                            <input type="text" name="subject" placeholder="Contact number *" class="form-control" required>
+                                        </div>
+                                    </div>
+									</div>
+									<div class="row gutter--20">
+                                    <div class="col-xs-6 col-xxs-12">
+                                        <div class="form-group">
+                                            <input type="text" name="name" placeholder="Whatsapp number *" class="form-control" required>
+                                        </div>
+                                    </div>
+									</div>
+									<div class="row gutter--20">
+                                    <div class="col-xs-6 col-xxs-12">
+                                        <div class="form-group">
+                                            <textarea name="message" placeholder="Address *" class="form-control" required></textarea>
+                                        </div>
+                                    </div>
+									</div>
+									<div class="row gutter--20">
+                                    <div class="col-xs-12">
+                                        <button type="submit" class="btn btn-primary mt--10">Register</button>
+                                    </div>
+                                </div>
+								
+
+                                <div class="status"></div>
+                            </form>
+                        </div>
+                        <!-- Contact Form End -->
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Contact Section End -->
+
+        <!-- Footer Section Start -->
+        <?php  include("footer.php"); ?>
+        <!-- Footer Section End -->
+   
+    <!-- Wrapper End -->
+
+    <!-- Back To Top Button Start -->
+    <div id="backToTop">
+        <a href="#" class="btn"><i class="fa fa-caret-up"></i></a>
+    </div>
+    <!-- Back To Top Button End -->
+
+    <!-- ==== Plugins Bundle ==== -->
+    <script data-cfasync="false" src="../../../cdn-cgi/scripts/ddc5a536/cloudflare-static/email-decode.min.js"></script><script src="js/plugins.min.js"></script>
+
+    <!-- ==== Color Switcher Plugin ==== -->
+   
+
+    <!-- ==== Main Script ==== -->
+    <script src="js/main.js"></script>
+
+</body>
+
+<!-- Mirrored from themelooks.us/demo/socifly/html/contact.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 01 Dec 2017 13:50:17 GMT -->
+</html>
